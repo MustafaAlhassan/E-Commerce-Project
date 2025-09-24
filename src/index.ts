7d990b66c4edc1d6f7d6e52420e047e169b8af2b
Mustafa Alhassan <mustafamusab266@gmail.com>
2025-09-24 10:49:42 +0300

    Product Feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import { seedInitialProducts } from "./services/productService";
 import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
 
 const app = express();
 const port = 3001;
@@ -12,7 +14,10 @@ mongoose
   .then(() => console.log("connected!!"))
   .catch((err) => console.log("No connection", err));
 
+seedInitialProducts();
+
 app.use("/user", userRoute);
+app.use("/product", productRoute)
 
 app.listen(port, () => {
   console.log(`Server is running at: http://localhost:${port}`);
---
+++ src/services/productService.ts
@@ -0,0 +1,21 @@
+import { productModel } from "../models/productModel";
+
+export const getAllProducts = async () => {
+  return await productModel.find();
+};
+
+export const seedInitialProducts = async () => {
+  const products = [
+    {
+      title: "Lenovo Laptop",
+      image:
+        "https://imgs.search.brave.com/VGN2C3eigWVo64LWYrUDtHWWm3_x9CoCTp_bkiuf4T0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/bW9zLmNtcy5mdXR1/cmVjZG4ubmV0L1dR/Vzg1cEJKWXFOS2JO/WmRWdnZISk0ucG5n",
+      price: 500000,
+      stock: 15,
+    },
+  ];
+  const existingProducts = await getAllProducts();
+  if (existingProducts.length === 0) {
+    await productModel.insertMany(products);
+  }
+};
