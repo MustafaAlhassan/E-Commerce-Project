090ef5882fb2847a0bc90750312562eebbd0849d
Mustafa Alhassan <mustafamusab266@gmail.com>
2025-10-06 18:44:03 +0300

    AuthContext + Register Page Integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Navbar from './components/Navbar'
-import RegisterPage from './pages/RegisterPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import RegisterPage from "./pages/RegisterPage";
+import AuthProvide from "./context/Auth/AuthProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={HomePage()}/>
-      <Route path="/register" element={RegisterPage()}/>
-    </Routes>
-    </BrowserRouter>
-  )
+    <AuthProvide>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/register" element={<RegisterPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvide>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,9 +1,9 @@
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { useRef, useState } from "react";
 import { BASE_URL } from "../constants/baseUrl";
+import { useAuth } from "../context/Auth/AuthContext";
 
 const RegisterPage = () => {
-
   const [error, setError] = useState("");
 
   const firstNameRef = useRef<HTMLInputElement>(null);
@@ -11,36 +11,47 @@ const RegisterPage = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
+  const { login } = useAuth();
+
   const onSubmit = async () => {
     const firstName = firstNameRef.current?.value;
     const lastName = lastNameRef.current?.value;
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
 
-    console.log(firstName, lastName, email, password)
-    const response = await fetch(`${BASE_URL}/user/register`, {
-      method: "post",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        firstName,
-        lastName, 
-        email,
-        password
-      })
-    });
-
-    if(!response.ok) {
-      setError("Unable to register user, please try different credientials!")
+    //Validate the form data
+    if (!firstName || !lastName || !email || !password) {
+      setError("Check Submitted Data")
       return;
     }
 
-    const data = await response.json();
-    console.log(data);
+    const response = await fetch(`${BASE_URL}/user/register`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
 
-  }
+    if (!response.ok) {
+      setError("Unable to register user, please try different credientials!");
+      return;
+    }
 
+    const token = await response.json();
+
+    if (!token) {
+      setError("Incorrect token");
+      return;
+    }
+
+    login(email, token); 
+  };
 
   return (
     <Container>
@@ -65,15 +76,31 @@ const RegisterPage = () => {
             border: 1,
             padding: 1,
             borderRadius: 2,
-            borderColor: "#d7d7d7ff"
+            borderColor: "#d7d7d7ff",
           }}
         >
-          <TextField inputRef={firstNameRef} label="First Name" name="firstName" />
+          <TextField
+            inputRef={firstNameRef}
+            label="First Name"
+            name="firstName"
+          />
           <TextField inputRef={lastNameRef} label="Last Name" name="lastName" />
-          <TextField inputRef={emailRef} label="Eamil" type="email" name="email" />
-          <TextField inputRef={passwordRef} label="Password" type="password" name="password" />
-          <Button onClick={onSubmit} variant="contained">Register</Button>
-          {error && <Typography sx={{color:"red"}}>{error}</Typography>}
+          <TextField
+            inputRef={emailRef}
+            label="Eamil"
+            type="email"
+            name="email"
+          />
+          <TextField
+            inputRef={passwordRef}
+            label="Password"
+            type="password"
+            name="password"
+          />
+          <Button onClick={onSubmit} variant="contained">
+            Register
+          </Button>
+          {error && <Typography sx={{ color: "red" }}>{error}</Typography>}
         </Box>
       </Box>
     </Container>
