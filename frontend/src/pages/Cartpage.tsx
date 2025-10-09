149b3d41fb367a73f283108d4d6ed6c0142798ca
Mustafa Alhassan <mustafamusab266@gmail.com>
2025-10-09 11:32:26 +0300

    Cart Page layout

--- frontend/src/pages/Cartpage.tsx
+++ frontend/src/pages/Cartpage.tsx
@@ -1,21 +1,46 @@
-import { Box, Container, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { BASE_URL } from "../constants/baseUrl";
-import { useAuth } from "../context/Auth/AuthContext";
+import { Box, Button, ButtonGroup, Container, Typography } from "@mui/material";
 import { useCart } from "../context/Cart/CartContext";
 
 const CartPage = () => {
-  const { token } = useAuth();
   const { cartItems, totalAmount } = useCart();
-  const [error, setError] = useState("");
-  
+
   return (
-    <Container>
+    <Container fixed sx={{ mt: 3, mb: 3 }}>
       <Typography variant="h4">My Cart</Typography>
-      {cartItems.map((item) => (
-        <Box>{item.title}</Box>
-      ))}
-      <Typography>{error}</Typography>
+      <Box display="flex" flexDirection="column" gap={2} mt={2}>
+        {cartItems.map((item) => (
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{
+              border: 1,
+              borderColor: "#dcdcdcff",
+              borderRadius: 2,
+              padding: 1,
+            }}
+          >
+            <Box display="flex" flexDirection="row" alignItems="center" gap={2}>
+              <img src={item.image} width={100} />
+              <Box>
+                <Typography variant="h6">{item.title}</Typography>
+                <Typography>
+                  {item.quantity} x {item.unitPrice.toLocaleString()} IQD
+                </Typography>
+                <Button sx={{color: "red"}}>REMOVE ITEM</Button>
+              </Box>
+            </Box>
+            <ButtonGroup variant="contained" aria-label="Basic button group">
+              <Button>-</Button>
+              <Button>+</Button>
+            </ButtonGroup>
+          </Box>
+        ))}
+        <Box>
+          <Typography variant="h5">Total Amount: {totalAmount.toLocaleString()} IQD</Typography>
+        </Box>
+      </Box>
     </Container>
   );
 };
